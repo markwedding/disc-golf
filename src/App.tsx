e6bdcc833e70506b57cd8a32d081ad3bed4a9b7d
Mark Wedding <markwedding10@gmail.com>
2020-06-22 21:15:25 -0400

    create basic setup with parcel

--- src/App.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function App() {
-  return <h1>Disc Golf</h1>;
-}
-
-export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,12 @@
+// index.tsx
 import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import * as serviceWorker from "./serviceWorker";
+import { render } from "react-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+// enable parcel HMR
+if (module.hot) {
+  module.hot.accept();
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+console.log("Hello from tsx!");
+
+render(<p>Hello</p>, document.getElementById("root"));
