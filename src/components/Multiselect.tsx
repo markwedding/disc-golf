31c364b5b1ae1c2dbbe64e7cb730117d8a3fa2a5
Mark Wedding <markwedding10@gmail.com>
2020-07-04 11:06:04 -0400

    start overriding react select control styles

--- src/components/Multiselect.tsx
+++ src/components/Multiselect.tsx
@@ -1,7 +1,7 @@
 import React, { ComponentProps } from 'react'
 import type { FC } from 'react'
 import ReactSelect, { components } from 'react-select'
-import { Icon, Box, Flex, useColorMode } from '@chakra-ui/core'
+import { Icon, Flex, useColorMode } from '@chakra-ui/core'
 import customTheme from 'src/theme'
 
 /**
@@ -28,13 +28,29 @@ import customTheme from 'src/theme'
 const Multiselect: FC<ComponentProps<typeof ReactSelect>> = (props) => {
   const { colorMode } = useColorMode()
   const {
-    colors: { gray, whiteAlpha },
+    colors: { white, gray, whiteAlpha, blackAlpha },
   } = customTheme
   const primaryColor = colorMode === 'light' ? gray[800] : whiteAlpha[800]
 
   return (
     <ReactSelect
       {...props}
+      styles={{
+        control: (provided, state) => ({
+          ...provided,
+          height: '2.5rem',
+          borderColor: colorMode === 'light' ? gray[200] : whiteAlpha[50],
+          paddingLeft: 'calc(1rem - 2px)',
+          backgroundColor: colorMode === 'light' ? white : whiteAlpha[100],
+          '&:hover': {
+            borderColor: colorMode === 'light' ? gray[300] : whiteAlpha[200],
+          },
+        }),
+        valueContainer: (provided, state) => ({
+          ...provided,
+          paddingLeft: 0,
+        }),
+      }}
       components={{
         ClearIndicator: ({ innerProps }) => (
           <Flex {...innerProps} mr={2}>
@@ -43,9 +59,9 @@ const Multiselect: FC<ComponentProps<typeof ReactSelect>> = (props) => {
         ),
         DropdownIndicator: (props) => (
           <components.DropdownIndicator {...props}>
-            <Box color={primaryColor}>
+            <Flex color={primaryColor}>
               <Icon name="chevron-down" size="1.25em" />
-            </Box>
+            </Flex>
           </components.DropdownIndicator>
         ),
       }}
